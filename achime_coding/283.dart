cd18d985751c00ec4bc12abe8e3082063944f460
Yujin C. <yujin.mar.chung@gmail.com>
2024-10-30 09:44:41 +0900

    updated variable names

--- achime_coding/283.dart
+++ achime_coding/283.dart
@@ -1,13 +1,13 @@
 /// Move Zeroes
 class Solution {
   void moveZeroes(List<int> nums) {
-    int non_zero = 0;
-    for (int yes_zero = 0; yes_zero<nums.length; yes_zero++) {
-        if (nums[yes_zero] != 0) {
-            int temp = nums[non_zero];
-            nums[non_zero] = nums[yes_zero];
-            nums[yes_zero] = temp;
-            non_zero++;
+    int lastNonZeroFoundAt = 0;
+    for (int cursor = 0; cursor<nums.length; cursor++) {
+        if (nums[cursor] != 0) {
+            int temp = nums[lastNonZeroFoundAt];
+            nums[lastNonZeroFoundAt] = nums[cursor];
+            nums[cursor] = temp;
+            lastNonZeroFoundAt++;
         }
     }
   } 
