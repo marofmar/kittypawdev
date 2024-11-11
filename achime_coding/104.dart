239d3be69758f72eb1ac68f7e2859bc8fff1bb47
Yujin C. <yujin.mar.chung@gmail.com>
2024-11-11 09:33:23 +0900

    24-11-11

---
+++ achime_coding/104.dart
@@ -0,0 +1,24 @@
+// Maximum depth of binary tree
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  int maxDepth(TreeNode? root) {
+    if (root == null) {
+        return 0;
+    }
+    int leftDepth = maxDepth(root.left);
+    int rightDepth = maxDepth(root.right);
+
+    int max = (leftDepth > rightDepth) ? leftDepth : rightDepth;
+    return 1 + max;
+  }
+  
+}
